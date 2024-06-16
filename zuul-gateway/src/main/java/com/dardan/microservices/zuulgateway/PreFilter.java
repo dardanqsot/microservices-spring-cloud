74c7fa13692ee68ed18246081397622e84c4712c
dardanqsot <quispesotodaniel@gmail.com>
2024-06-16 08:17:38 -0500

    Add zuul gateway project

---
+++ zuul-gateway/src/main/java/com/dardan/microservices/zuulgateway/PreFilter.java
@@ -0,0 +1,41 @@
+package com.dardan.microservices.zuulgateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class PreFilter extends ZuulFilter {
+
+//    private static final Logger log = LoggerFactory.getLogger(PreFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        log.info(String.format("%s request router to %s", request.getMethod(), request.getRequestURL().toString()));
+        Long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return null;
+    }
+}
