3bcc6e718cef2231cd51281dd968d814de296f3a
dardanqsot <quispesotodaniel@gmail.com>
2024-06-28 22:26:38 -0500

    add authentication-server-oauth2

---
+++ authentication-server-oauth2/src/main/java/com/dardan/microservices/authenticationserveroauth2/AuthenticationServerOauth2Application.java
@@ -0,0 +1,13 @@
+package com.dardan.microservices.authenticationserveroauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServerOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServerOauth2Application.class, args);
+    }
+
+}
---
+++ authentication-server-oauth2/src/test/java/com/dardan/microservices/authenticationserveroauth2/AuthenticationServerOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dardan.microservices.authenticationserveroauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
