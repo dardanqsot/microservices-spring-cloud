5052d0e519278251ed93d08532629b06b83547fe
dardanqsot <quispesotodaniel@gmail.com>
2024-07-05 23:02:11 -0500

    add authentication-server-jwt project

---
+++ authentication-server-jwt/src/test/java/com/dardan/microservices/authenticationserverjwt/AuthenticationServerJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dardan.microservices.authenticationserverjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
