56a5c40e4f188dc6c3f2691595045ace1f6438f8
dardanqsot <quispesotodaniel@gmail.com>
2025-04-26 08:55:03 -0500

    add product service command and product query service to CQRS pattern

---
+++ product-service-command/src/main/java/com/dardan/microservices/productservicecommand/ProductServiceCommandApplication.java
@@ -0,0 +1,13 @@
+package com.dardan.microservices.productservicecommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceCommandApplication.class, args);
+    }
+
+}
